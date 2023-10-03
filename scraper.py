8cf8685766dbf7193f730dbb6189d928ac6e37c9
Alex LASNIER <133754892+alasnier@users.noreply.github.com>
2023-10-03 17:45:54 +0200

    Collect of races results ✅
    Next step : Collect circuits coordinates

--- scraper.py
+++ scraper.py
@@ -1,203 +1,191 @@
-########################################################################################################################
-import pandas as pd
 import requests
+import pandas as pd
 import json
-from google.cloud import bigquery
+import time
+from datetime import datetime
+
+# Start timer
+startTime = time.time()
+
+# Print timestamp
+print(f"--- *** {datetime.now()} *** ---")
+print("\n")
+
 
 ########################################################################################################################
-# Scrap all MotoGP standings from all seasons
+# RUN
 ########################################################################################################################
-# Scrap all seasons IDs
-response = requests.get("https://api.motogp.pulselive.com/motogp/v1/results/seasons")
-
-print()
-# Check if the request was successful
-if response.status_code == 200:
-    data = response.json()  # Parse the JSON response directly
-
-    seasons_df = pd.DataFrame(data)  # Create a DataFrame from the JSON data
-    seasons_df = seasons_df[["id", "year"]]
-    seasons_ids = seasons_df["id"].tolist()
-
-    print("Seasons successfully processed:")
-    print(f"This is the list: {seasons_ids}")
-else:
-    print("Failed to retrieve the page.")
-
-# Scrap MotoGP category ID
-response = requests.get(
-    "https://api.motogp.pulselive.com/motogp/v1/results/categories?seasonUuid=db8dc197-c7b2-4c1b-b3a4-7dc723c087ed"
-)
-
-print()
-# Check if the request was successful
-if response.status_code == 200:
-    try:
-        json_data = response.json()
-        print("JSON data retrieved successfully")
-        print()
-
-        # Find MotoGP category and extract its ID
-        for category in json_data:
-            if category["name"] == "MotoGP™":
-                motogp_id = category["id"]
-                print(f"MotoGP id = {motogp_id}")
-                print()
-                break
-    except json.JSONDecodeError:
-        print("Failed to parse JSON response.")
-        print()
-else:
-    print("Failed to retrieve the page.")
-    print()
-
-########################################################################################################################
-# Scrap all MotoGP seasons championships
-
-# Initialize an empty list to hold all data
-all_data = []
-
-# Loop through seasons IDs and retrieve MotoGP standings
-count = 0
-for season_id in seasons_ids:
-    count = count + 1
-    url = f"https://api.motogp.pulselive.com/motogp/v1/results/standings?seasonUuid={season_id}&categoryUuid={motogp_id}"
-    response = requests.get(url)
-
-    print(
-        f"Season ID: {season_id}, Status Code: {response.status_code}, Count = {count}"
-    )
-    print()
-
-    if response.status_code == 200:
-        json_data = response.json()
-
-        # Extract relevant information from the JSON response
-        data = []
-        for entry in json_data.get("classification", []):
-            rank = entry.get("position")
-            rider_name = entry.get("rider", {}).get("full_name")
-            country = entry.get("rider", {}).get("country", {}).get("iso")
-
-            # Check if the "team" object exists before accessing its attributes
-            team_info = entry.get("team")
-            team_name = team_info.get("name") if team_info else "No Team"
-
-            year = None
-            if team_info and "season" in team_info:
-                year = entry.get("team", {}).get("season", {}).get("year")
-
-            constructor = entry.get("constructor", {}).get("name")
-
-            data.append([rank, rider_name, country, team_name, constructor, year])
-
-        # Append the data for the current season to the all_data list
-        all_data.extend(data)
-
-    else:
-        print("Failed to retrieve standings for season", season_id)
-
-# Create a DataFrame from the collected data
-columns = ["Rank", "Rider Name", "Country", "Team Name", "Constructor", "Year"]
-seasons_rankings_df = pd.DataFrame(all_data, columns=columns)
-
-# Print the global DataFrame or do whatever you need with it
-print()
-print(seasons_rankings_df)
-print()
-
-########################################################################################################################
-# Scrap MotoGP race results from all seasons
-########################################################################################################################
-# Define the API URLs
+# Define the API endpoint for seasons
 seasons_url = "https://api.motogp.pulselive.com/motogp/v1/results/seasons"
-events_url = "https://api.motogp.pulselive.com/motogp/v1/results/events"
-results_url = "https://api.motogp.pulselive.com/motogp/v1/results/session/e38536d9-bf81-4613-b0bf-91ec5ea13de1/classification?test=false"
+# Request data from the API
+seasons_response = requests.get(seasons_url).json()
+# Create a dictionary with season years as keys and their IDs as values
+seasons_dict = {str(season["year"]): season["id"] for season in seasons_response}
 
-# Fetch data from the API
-seasons_response = requests.get(seasons_url)
-seasons_data = seasons_response.json()
+# Filter seasons that contain "202"
+filtered_dict = {key: value for key, value in seasons_dict.items() if "202" in key}
 
-# Create an empty list to store data
-results_data_list = []
+# Find the MotoGP category ID
+motogp_id = None
+# Define the API endpoint for categories
+categories_url = "https://api.motogp.pulselive.com/motogp/v1/results/categories?eventUuid=14bd179e-a4bc-4b0d-bd2e-69a9ed99cb60"
+# Request data from the API
+categories_response = requests.get(categories_url).json()
 
-# Loop through each season
-for season in seasons_data:
-    season_year = season["year"]
+# Find MotoGP category ID
+for category in categories_response:
+    if "MotoGP" in category["name"]:
+        motogp_id = category["id"]
+        break
 
-    # Fetch events data for the current season
-    events_params = {"seasonUuid": season["id"], "isFinished": True}
-    events_response = requests.get(events_url, params=events_params)
-    events_data = events_response.json()
+# Initialize empty lists for event data, race data, and championship rank data
+all_events_data = []
+all_races_data = []
+championship_ranks_data = []
 
-    # Loop through each event (race) in the current season
-    for event in events_data:
-        circuit_name = event["circuit"]["name"]
+# Iterate through each season and fetch event data
+for season_year, season_id in filtered_dict.items():
+    # Define the API endpoint for events of a specific season
+    events_url = f"https://api.motogp.pulselive.com/motogp/v1/results/events?seasonUuid={season_id}&isFinished=true"
+    # Request data from the API
+    events_response = requests.get(events_url).json()
 
-        # Fetch results data for the current event
-        results_response = requests.get(results_url)
-        results_data = results_response.json()["classification"]
+    # Create a DataFrame with relevant columns from events_response
+    events_data = []
+    for event in events_response:
+        events_data.append(
+            {
+                "season_year": season_year,
+                "event_name": event["name"],
+                "event_id": event["id"],
+                "country": event.get("country", {}).get("name"),
+                "circuit_name": event.get("circuit", {}).get("name"),
+            }
+        )
 
-        # Loop through each result in the current event
-        for result in results_data:
-            rank = result["position"]
-            rider_name = result["rider"]["full_name"]
-            nationality = result["rider"]["country"]["name"]
-            team_name = result["team"]["name"]
-            constructor_name = result["constructor"]["name"]
+    # Append the event data for this season to the overall list
+    all_events_data.extend(events_data)
 
-            # Append the data to the list
-            results_data_list.append(
+# Create a DataFrame containing information from all seasons
+events_df = pd.DataFrame(all_events_data)
+
+# Filter and drop rows where "TEST" is in the "event_name" column
+events_df = events_df[~events_df["event_name"].str.contains("TEST")]
+
+# Reset the index
+events_df.reset_index(drop=True, inplace=True)
+
+# Iterate through each event and fetch race data
+for event_id in events_df["event_id"]:
+    # Define the API endpoint for race sessions of a specific event
+    events_types_url = f"https://api.motogp.pulselive.com/motogp/v1/results/sessions?eventUuid={event_id}&categoryUuid={motogp_id}"
+
+    # Request data from the API
+    events_types_response = requests.get(events_types_url).json()
+
+    # Filter and extract data for races (type == "RAC")
+    races_data = []
+    for session in events_types_response:
+        if session["type"] == "RAC":
+            races_data.append(
                 {
-                    "Season": season_year,
-                    "Circuit": circuit_name,
-                    "Rank": rank,
-                    "Rider": rider_name,
-                    "Nationality": nationality,
-                    "Team": team_name,
-                    "Constructor": constructor_name,
+                    "event_id": event_id,
+                    "circuit_name": session["circuit"],
+                    "race_id": session["id"],
                 }
             )
 
-# Create a DataFrame from the list of data
-races_rankings_df = pd.DataFrame(results_data_list)
+    # Append the race data for this event to the overall list
+    all_races_data.extend(races_data)
 
-# Display the resulting DataFrame
-print(races_rankings_df)
+# Create a DataFrame containing race information from all events
+events_types_df = pd.DataFrame(all_races_data)
+
+# Merge event_df and events_types_df on 'event_id'
+races_df = events_df.merge(events_types_df, on="event_id", how="left")
+
+# Rename columns and drop duplicates
+races_df = races_df.drop(["circuit_name_y"], axis=1)
+races_df = races_df.rename(columns={"circuit_name_x": "circuit_name"})
+races_df = races_df.drop_duplicates()
+races_df.reset_index(drop=True, inplace=True)
+
+# Define the base URL for fetching championship ranks
+base_url = "https://api.motogp.pulselive.com/motogp/v1/results/session"
+
+# Iterate through each race_id in races_df
+for race_id in races_df["race_id"]:
+    # Define the API endpoint for fetching championship ranks
+    ranks_url = f"{base_url}/{race_id}/classification?test=false"
+
+    try:
+        # Request data from the API
+        ranks_response = requests.get(ranks_url)
+        ranks_response.raise_for_status()  # Raise an error for non-OK responses
+
+        # Parse the JSON response
+        ranks_data = ranks_response.json()
+
+        # Iterate through the ranks in the response and extract relevant information
+        for rank in ranks_data.get("classification", []):
+            rider_info = rank.get("rider", {})
+            country_info = rider_info.get("country", {})
+            team_info = rank.get("team", {})
+            constructor_info = rank.get("constructor", {})
+
+            championship_ranks_data.append(
+                {
+                    "season_year": races_df.loc[
+                        races_df["race_id"] == race_id, "season_year"
+                    ].values[0],
+                    "event_name": races_df.loc[
+                        races_df["race_id"] == race_id, "event_name"
+                    ].values[0],
+                    "event_id": races_df.loc[
+                        races_df["race_id"] == race_id, "event_id"
+                    ].values[0],
+                    "circuit_country": races_df.loc[
+                        races_df["race_id"] == race_id, "country"
+                    ].values[0],
+                    "circuit_name": races_df.loc[
+                        races_df["race_id"] == race_id, "circuit_name"
+                    ].values[0],
+                    "race_id": race_id,
+                    "rider_name": rider_info.get("full_name", ""),
+                    "rider_country": country_info.get("name", ""),
+                    "team_name": team_info.get("name", ""),
+                    "constructor_name": constructor_info.get("name", ""),
+                    "position": rank.get("position", ""),
+                }
+            )
+    except requests.exceptions.HTTPError as e:
+        # Handle HTTP error, such as 404 Not Found or 500 Internal Server Error
+        print(f"HTTP Error: {e}")
+    except json.JSONDecodeError as e:
+        # Handle JSON decode error (invalid JSON response)
+        print(f"JSON Decode Error: {e}")
+    except Exception as e:
+        # Handle other exceptions
+        print(f"An error occurred: {e}")
+
+# Create the championship_ranks_df DataFrame, drop duplicates, and reset the index
+championship_ranks_df = pd.DataFrame(championship_ranks_data)
+championship_ranks_df = championship_ranks_df.drop_duplicates()
+championship_ranks_df.reset_index(drop=True, inplace=True)
+
+# Export data to CSV
+championship_ranks_df.to_csv("motogp_datas.csv", index=False)
+
 
 ########################################################################################################################
-# Upload to GCP BigQuery
+# TIMER
 ########################################################################################################################
-# Construct a BigQuery client object.
-client = bigquery.Client()
-# Set table_id to the ID of the table to create.
-table_id_races = "motogp-project-398408.motogp_dataset.motogp_races_results"
-table_id_seasons = "motogp-project-398408.motogp_dataset.motogp_seasons_results"
-
-job_config = bigquery.LoadJobConfig()
-
-# RACES
-job_races = client.load_table_from_dataframe(
-    races_rankings_df, table_id_races, job_config=job_config
-)  # Make an API request.
-job_races.result()  # Wait for the job to complete.
-
-table_races = client.get_table(table_id_races)  # Make an API request.
+endTime = time.time()
+executionTime = endTime - startTime
+print("")
+print("")
 print(
-    "Loaded {} rows and {} columns to {}".format(
-        table_races.num_rows, len(table_races.schema), table_id_races
-    )
+    f"--- *** Total execution time in seconds: {executionTime}, in minutes: {executionTime/60} and in hours: {executionTime/60/60} *** ---"
 )
-
-# SEASONS
-job_seasons = client.load_table_from_dataframe(
-    seasons_rankings_df, table_id_seasons, job_config=job_config
-)  # Make an API request.
-job_seasons.result()  # Wait for the job to complete.
-
-table_seasons = client.get_table(table_id_seasons)  # Make an API request.
-print(
-    "Loaded {} rows and {} columns to {}".format(
-        table_seasons.num_rows, len(table_seasons.schema), table_id_races
-    )
-)
+print("")
+print("")
